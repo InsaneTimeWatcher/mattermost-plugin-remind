07d7ab9210609b5b32f5050090f08fd42cefb79c
Pierre de La Morinerie <kemenaran@gmail.com>
2022-05-09 12:28:04 +0000

    server: catch up on missed ticks
    Catches up ticks up to 10 minutes (as longer may overload the server
    when starting up after being stopped a very long time).
    
    If it takes longer to catch up for ticks that a tick, the missed
    ticks will be caught up on the next turn.

--- server/utils.go
+++ server/utils.go
@@ -7,6 +7,14 @@ import (
 	"github.com/nicksnyder/go-i18n/i18n"
 )
 
+func MaxTime(a time.Time, b time.Time) time.Time {
+	if (a.Before(b)) {
+		return b
+	} else {
+		return a
+	}
+}
+
 func (p *Plugin) translation(user *model.User) (i18n.TranslateFunc, string) {
 	locale := "en"
 	for l := range p.locales {
